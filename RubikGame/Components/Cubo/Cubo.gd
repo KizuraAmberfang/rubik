a4ef4af30a9dc83712fa25f50fadb18c6e7c9506
Giulia Di lorenzo <gdi-lore@e3r10p3.42roma.it>
2022-02-18 19:20:42 +0100

    continuo a costruire il cubo

--- RubikGame/Components/Cubo/Cubo.gd
+++ RubikGame/Components/Cubo/Cubo.gd
@@ -10,15 +10,68 @@ export var down : Color
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CentralPieceO.faceColor = front
-	$SpigoloOG.faceColorB = front
-	$SpigoloOG.faceColorA = right
-	$SpigoloBO.faceColorA = front
-	$SpigoloBO.faceColorB = left
+	$CentralPieceG.faceColor = right
+	$CentralPieceW.faceColor = upper
+	$CentralPieceB.faceColor = left
+	$CentralPieceR.faceColor = back
+	$CentralPieceY.faceColor = down
+	
+	$SpigoloOG.faceColorR = front
+	$SpigoloOG.faceColorB = right
+	$SpigoloBO.faceColorB = front
+	$SpigoloBO.faceColorR = left
+	$SpigoloOY.faceColorB = front
+	$SpigoloOY.faceColorR = down
+	$SpigoloWO.faceColorR = front
+	$SpigoloWO.faceColorB = upper
+	$SpigoloGW.faceColorB = right
+	$SpigoloGW.faceColorR = upper
+	$SpigoloGY.faceColorB = down
+	$SpigoloGY.faceColorR = right
+	$SpigoloGR.faceColorB = back
+	$SpigoloGR.faceColorR = right
+	
+	$VerticeOWB.faceColorB = upper
+	$VerticeOWB.faceColorG = left
+	$VerticeOWB.faceColorR = front
+	$VerticeWOG.faceColorB = right
+	$VerticeWOG.faceColorG = upper
+	$VerticeWOG.faceColorR = front
+	$VerticeOGY.faceColorB = front
+	$VerticeOGY.faceColorG = down
+	$VerticeOGY.faceColorR = right
+	$VerticeOBY.faceColorB = front
+	$VerticeOBY.faceColorG = left
+	$VerticeOBY.faceColorR = down
+	$VerticeRGY.faceColorB = right
+	$VerticeRGY.faceColorG = down
+	$VerticeRGY.faceColorR = back
+	$VerticeWGR.faceColorB = upper
+	$VerticeWGR.faceColorG = right
+	$VerticeWGR.faceColorR = back
+	
 	
 	$CentralPieceO.change()
+	$CentralPieceG.change()
+	$CentralPieceW.change()
+	$CentralPieceB.change()
+	$CentralPieceR.change()
+	$CentralPieceY.change()
+	
 	$SpigoloOG.change()
 	$SpigoloBO.change()
-
+	$SpigoloOY.change()
+	$SpigoloWO.change()
+	$SpigoloGW.change()
+	$SpigoloGY.change()
+	$SpigoloGR.change()
+	
+	$VerticeOWB.change()
+	$VerticeWOG.change()
+	$VerticeOGY.change()
+	$VerticeOBY.change()
+	$VerticeRGY.change()
+	$VerticeWGR.change()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- RubikGame/Components/Vertice/Vertice.gd
+++ RubikGame/Components/Vertice/Vertice.gd
@@ -1,20 +1,23 @@
 extends RigidBody
 
-onready var faceColorB : Color
+onready var faceColorG : Color
 onready var faceColorR : Color
+onready var faceColorB : Color
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
 func change():
-	var matB = SpatialMaterial.new()
-	matB.albedo_color = faceColorB
-	$FacciaB.set_material_override(matB)
+	var matG = SpatialMaterial.new()
+	matG.albedo_color = faceColorG
+	$FacciaG.set_material_override(matG)
 	var matR = SpatialMaterial.new()
 	matR.albedo_color = faceColorR
 	$FacciaR.set_material_override(matR)
-
+	var matB = SpatialMaterial.new()
+	matB.albedo_color = faceColorB
+	$FacciaB.set_material_override(matB)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
