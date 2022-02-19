bcb51289664f402fdc47c1120f7a440ad9245fb4
gdi-lore <diana.blackhurst@gmail.com>
2022-02-19 23:52:30 +0100

    starting setting the pos

--- cubepos/cubepos.hpp
+++ cubepos/cubepos.hpp
@@ -10,7 +10,82 @@ extern const class cubepos identity_cube; // identity of the group
 
 class cubepos 
 {
-    public: // in lesson 9
+    public: 
+    // function for comparing two cube - lesson 9
+    inline bool operator< (const cubepos &cp) const
+    {
+        return (memcmp(this, &cp, sizeof(cp)) < 0);
+    }
+
+    inline bool operator= (const cubepos &cp) const
+    {
+        return (memcmp(this, &cp, sizeof(cp)) = 0);
+    }
+
+    inline bool operator!= (const cubepos &cp) const
+    {
+        return (memcmp(this, &cp, sizeof(cp)) != 0);
+    }
+
+    // function to find the permutation and orientation of cubies
+    static inline int edge_perm(int cubieval)
+    {
+        // permutation of the edge: bitshifting of 1
+        // example: [xxx 1001 1] -> [xxxx 1001] -> 9, this is the slot
+        return (cubieval >> 1);
+    }
+
+    static inline int edge_ori(int cubieval)
+    {
+        // orientation of the edge: and with 1 [0000000 1]
+        // example: [xxx 1001 1] & [0000000 1] = [0000000 1] -> is flipped
+        return (cubieval & 1);
+    }
+
+    static inline int corner_perm(int cubieval)
+    {
+        // permutation of the corner: and with 7 [00000 111]
+        // example: [xxx 10 010] & [00000 111] = [00000 010] -> slot 2
+        return (cubieval & 7);
+    }
+
+    static inline int corner_ori(int cubieval)
+    {
+        // orientation of the corner: 3 bitshifting
+        // example: [xxx 10 010] >> 3 = [000000 10] -> 2, counterclowise twist
+        return (cubieval >> 3);
+    }
+
+    static inline int edge_flip(int cubieval)
+    {
+        // change flip orientation of the edge with xor operation
+        // example: [xxx 1001 1] xor [0000000 1] = [xxx 1001 0]
+        return (cubieval ^ 1);
+    }
+
+    static inline int edge_val(int perm, int ori)
+    {
+        // build the edge from his slot (perm) and orientation
+        // example: slot 3 and ori 1 -> 3 * 2 + 1 = 7 -> [000 0011 1]
+        return (perm * 2 + ori);
+    }
+
+    static inline int corner_val(int perm, int ori)
+    {
+        // build the corner from his slot (perm) and orientation
+        // example: slot 5 and ori 2 -> 2 * 8 + 5 = 21 -> [000 10 101]
+        return (ori * 8 + perm);
+    }
+
+    static inline edge_ori_add(int cv1, int cv2)
+    {
+        // give the orientation of cubieval (cv2) to cv1
+        // example: [000 0100 1] cv1 [000 0011 0] cv2
+        //          edge_ori(cv2) = 0
+        //          [000 0100 0] ^ [000 0000 0]
+        return (cv1 ^ edge_ori(cv2));
+    }
+
     // static data in lesson 12
 
     // ***  DATA RAPRESENTATION *** lesson 7
@@ -23,6 +98,8 @@ class cubepos
     //   2 - 3         6 - 7
 
     // the 4th and 5th bit is the orientation: 0 no twist, 1 clockwise twist, 2 counterclockwise twist
+    // example: [xxx 00 000] -> slot 0, no twist
+    //          [xxx 10 010] -> slot 2, counterclockwise twist
 
     unsigned char c[8] // this rapresent the 8 corner
 
@@ -34,6 +111,9 @@ class cubepos
     //  / 0 \          4 - 5         /  8  \
     //  1   2          |   |         9     10
     //  \ 3 /          6 - 7         \  11 /   
+
+    // example: [xxx 00 000] -> slot 0, no flip
+    //          [xxx 1001 1] -> slot 9, flip
 
     unsigned char e[12] // the 12 edges
 };
