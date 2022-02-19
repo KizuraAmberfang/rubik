327e846399903d87c53b5841684bdd4810995117
gdi-lore <diana.blackhurst@gmail.com>
2022-02-19 22:58:00 +0100

    starting with rapresentation

---
+++ cubepos/cubepos.hpp
@@ -0,0 +1,20 @@
+#ifndef CUBEPOS_HPP
+#define CUBEPOS_HPP
+const int NMOVES = 18 //F F2 F' etc
+const int TWIST = 3 // ?
+const int FACES = 6 // number of faces of the cube
+const int M = 48 // number of automorphism induced by rotation and reflection
+const int CUBIES = 24 // ?
+
+extern const class cubepos identity_cube; // identity of the group
+
+class cubepos 
+{
+    public: // in lesson 9
+    // static data in lesson 12
+    // data rapresentation in lesson 7
+
+};
+
+
+#endif
